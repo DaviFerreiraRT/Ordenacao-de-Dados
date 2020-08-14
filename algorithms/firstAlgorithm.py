7f5c10214550389d5fe5c20aca0c2821e802f1d6
Davi Ferreira <daviferreirart@gmail.com>
2020-08-14 15:00:34 -0300

    Update

--- algorithms/firstAlgorithm.py
+++ algorithms/firstAlgorithm.py
@@ -1,6 +1,6 @@
-#Algoritmo de soma
+# Algoritmo de soma
 soma = 0
-valor=float(input("Digite um valor para somar ou 0 para parar: "))
+valor = float(input("Digite um valor para somar ou 0 para parar: "))
 
 while valor != 0:
 
@@ -8,9 +8,9 @@ while valor != 0:
     print(f"Soma atual: {soma}")
     valor = float(input("Digite um valor para somar ou 0 para parar:"))
 if soma > 10:
-   
-    print("O resultado da soma foi acima de 10, a soma foi : ",soma)
+
+    print("O resultado da soma foi acima de 10, a soma foi : ", soma)
 elif soma < 10:
-    print("O resultado da soma foi abaixo de 10, a soma foi: ",soma)
+    print("O resultado da soma foi abaixo de 10, a soma foi: ", soma)
 else:
-     print("Soma é igual a 10")
+    print("Soma é igual a 10")
