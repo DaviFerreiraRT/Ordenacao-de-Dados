462f3bcf4f5cd42a93e97091a342d8c8f92be5a0
Davi Ferreira <daviferreirart@gmail.com>
2020-08-14 14:47:44 -0300

    PrimeiroCodigo

--- algorithms/firstAlgorithm.py
+++ algorithms/firstAlgorithm.py
@@ -1,15 +1,14 @@
-soma=0
-valor(float(input("Digite um 'valor'para 0 ou para parar: ")))
+soma = 0
+valor=(float(input("Digite um 'valor'para 0 ou para parar: ")))
 
-while valor !=0:
+while valor != 0:
 
-    soma+=valor
+    soma += valor
     print(f"Soma atual: {soma}")
-    valor=float(input("Digite um 'valor' ou 0 para parar:" ))
-
-        if soma>10:
-            print("Soma é maior do que 10")
-        elif soma<10:
-            print("Soma é menor do que 10")
-            else:
-                print("Soma é igual a 10")
+    valor = float(input("Digite um 'valor' ou 0 para parar:"))
+if soma > 10:
+    print("Soma é maior que 10")
+elif soma < 10:
+    print("Soma é menor do que 10")
+else:
+     print("Soma é igual a 10")
