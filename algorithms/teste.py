82ab6f426ccd09b951b682096f60f5016921e0e5
Davi Ferreira <daviferreirart@gmail.com>
2020-08-15 23:06:45 -0300

    Update

--- algorithms/teste.py
+++ algorithms/teste.py
@@ -1,1 +1,10 @@
-import firstAlgorithm
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def Teste(self):
+        print("Meu nome é "+ self.nome+", tenho" ,self.idade ,"anos")
+
+p1 = Pessoa("Davi",20)
+p1.Teste()
